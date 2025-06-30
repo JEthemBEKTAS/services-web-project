ee9cb0fcd92e6923c2b5b70b713d4fb56c618b4c
JEthemBEKTAS <jeanethem@hotmail.fr>
2025-07-01 00:52:06 +0200

    modification fichiers docker/mariadb/Dockerfile

--- docker/mariadb/init/init.sql
+++ docker/mariadb/init/init.sql
@@ -1,4 +1,12 @@
+-- Création de la base et de l’utilisateur avec droits limités
 CREATE DATABASE wordpress_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER 'wp_user'@'%' IDENTIFIED BY 'motdepasse_securise';
-GRANT SELECT, INSERT, UPDATE, EXECUTE ON wordpress_db.* TO 'wp_user'@'%';
+
+-- Privilèges nécessaires à WordPress (pas de DROP DATABASE global)
+GRANT 
+  SELECT, INSERT, UPDATE, DELETE,
+  CREATE, DROP, ALTER, INDEX,
+  CREATE TEMPORARY TABLES
+ON wordpress_db.* TO 'wp_user'@'%';
+
 FLUSH PRIVILEGES;
