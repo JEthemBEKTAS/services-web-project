8a2c7b960e3c8b904fc6f0d5b7596a2288be6b7f
JEthemBEKTAS <jeanethem@hotmail.fr>
2025-08-14 22:42:02 +0200

    version locale finale a tester

--- docker/mariadb/entrypoint/docker-entrypoint.sh
+++ docker/mariadb/entrypoint/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Si le template existe, générer init.sql
 if [ -f /docker-entrypoint-initdb.d/init.sql.tpl ]; then
-  envsubst '${MYSQL_ROOT_PASSWORD}' \
+  envsubst '${MYSQL_ROOT_PASSWORD} ${MYSQL_WP_PASSWORD}' \
     < /docker-entrypoint-initdb.d/init.sql.tpl \
     > /docker-entrypoint-initdb.d/init.sql
 fi
